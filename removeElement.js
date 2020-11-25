ab79da9d1fd795acc1f6010c6ff4bd29e071c2ba
Royalthius <arathurs@gmail.com>
2020-11-24 22:16:05 -0500

    Comments added for enhanced readability.

--- removeElement.js
+++ removeElement.js
@@ -5,9 +5,16 @@
  */
 const removeElement = (nums, val) => {
     
+    // Iterate through the array using a 
+    // reverse for loop
     for (let i = nums.length - 1; i >= 0; i--) {
+        
+        // If nums[i] is equal to val, remove
+        // nums[i] from the nums array in-place,
+        // using the splice function
         if(nums[i] === val) nums.splice(i, 1);
     }
     
+    // Return final length
     return nums.length;
 };
